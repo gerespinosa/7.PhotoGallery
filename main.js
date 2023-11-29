bdfcb064f0f4bbde0a9f0b90500c40c8c3b951ce
Ger Espinosa <espinosa.toscano@gmail.com>
2023-11-29 12:58:41 +0100

    Firts commit

---
+++ main.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  const modal = document.createElement("div");
+  modal.id = "modal";
+  document.body.appendChild(modal);
+
+  const images = document.querySelectorAll(".img");
+
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      modal.classList.add("active");
+      const img = document.createElement("img");
+      img.src = image.src;
+      img.id = "img";
+
+      while (modal.firstChild) {
+        modal.removeChild(modal.firstChild);
+      }
+      modal.append(img);
+    });
+  });
+};
